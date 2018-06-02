05018ed2e97b53c1885fa46c3577ddb9b25d3671
BigInteger28 <joris28vn@gmail.com>
2018-06-02 20:00:46 +0200

    Testing github shizzle

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        //Robin rules the world with BiggerIntegers
+        //Robin rules the world with BiggerBigIntegers
     }
 }
