e5bb1fd0f73556a0d6c3e610d335737639c2e94e
BigInteger28 <joris28vn@gmail.com>
2018-06-02 23:39:22 +0200

    New class Engine

---
+++ src/backend/Engine.java
@@ -0,0 +1,15 @@
+package backend;
+
+public class Engine {
+    private final int[] DEPTHS;
+    private int currentmove;
+
+    public Engine() {
+        DEPTHS = new int[]{0, 17, 36, 12, 94, 28, 147, 12, 7}; //TODO: depths uit een file laden
+        currentmove = 0;
+    }
+
+    public int getMove(int playermove) {
+        return (playermove + DEPTHS[currentmove]) % 4;
+    }
+}
