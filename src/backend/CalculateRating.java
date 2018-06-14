ff125ebe6d5bd06fc0284a8b0cec4fc3746cce94
BigInteger28 <joris28vn@gmail.com>
2018-06-14 23:15:47 +0200

    Prestige rating system calculator

--- src/backend/CalculateRating.java
+++ src/backend/CalculateRating.java
@@ -1,46 +1,13 @@
 package backend;
 
+import static java.lang.Math.round;
+
 public class CalculateRating {
 
-    private final String[] BELTS = {"White", "Grey", "Yellow", "Orange", "Lime", "Green", "Turquoise", "Blue", "Dark Blue", "Magenta", "Purple", "Brown", "Red", "Black"};
-
-
-    private int first;
-    private int second;
-    private boolean isFirstHighest;
-
-    public CalculateRating(int firstTotalStars, int secondTotalStars) {
-        first = firstTotalStars;
-        second = secondTotalStars;
-        if (first > second) {
-            isFirstHighest = true;
-        } else {
-            isFirstHighest = false;
-        }
+    public float getPrestigeRating(float sumtotrat, float matches, float score, float highestRatWon, float lowestRatLose, float highestRatPlayed, float lowestRatPlayed) {
+        float rating = (((sumtotrat / matches) + ((highestRatWon + lowestRatLose) / 2)) / 2) + ((score - (matches / 2)) * ((highestRatPlayed - lowestRatPlayed) / matches));
+        return round(rating);
     }
 
-    private void calculateRatings() {
 
-
-    }
-
-    private void calculateHighest(int rating, int other) {
-
-    }
-
-    private void calculateLowest(int rating, int other) {
-
-    }
-
-    private void calculatePlacement() {
-
-    }
-
-    public int getFirst() {
-
-    }
-
-    public int getSecond() {
-
-    }
 }
