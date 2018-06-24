39182ea1170ff31fb4f104952e056d4f22e20b4d
yugecin <yugecin@users.noreply.github.com>
2018-06-24 20:32:05 +0200

    use gamedata as much as possible in engines

--- src/engines/DepthEngine.java
+++ src/engines/DepthEngine.java
@@ -3,8 +3,6 @@ package engines;
 import backend.Game.Data;
 import backend.Player;
 
-import java.util.Arrays;
-
 import static common.Constants.*;
 
 public class DepthEngine implements Player
@@ -12,9 +10,7 @@ public class DepthEngine implements Player
     private final int playerNumber;
     private final String name;
     private final byte[] depths;
-    private final int[] elementsLeft;
 
-    private int otherplayerelement;
     private Database db;
 
     public DepthEngine(int playerNumber, String name, byte[] depths)
@@ -22,7 +18,6 @@ public class DepthEngine implements Player
         this.playerNumber = playerNumber;
         this.name = name;
         this.depths = depths;
-        this.elementsLeft = new int[5];
     }
 
     @Override
@@ -32,20 +27,19 @@ public class DepthEngine implements Player
     }
 
     @Override
-    public int doMove(Data gamedata)
+    public int doMove(Data data)
     {
-        final int move = gamedata.getCurrentMove();
+        final int move = data.getCurrentMove();
         if (move == 0) {
             return DEFENSE;
         }
         
-        int dbres = db.findEntry(gamedata, this.playerNumber);
-        if (dbres != -1 && this.elementsLeft[dbres] > 0) {
-            this.elementsLeft[dbres]--;
+        int dbres = db.findEntry(data, this.playerNumber);
+        if (dbres != -1 && data.getElementsLeft(this.playerNumber, dbres) > 0) {
             return dbres;
         }
         
-        int element = this.otherplayerelement;
+        int element = data.getMove(this.playerNumber ^ 1, move - 1);
         if (element == DEFENSE) {
             element = AIR; // see engine.vb:53
         }
@@ -57,10 +51,9 @@ public class DepthEngine implements Player
         onedepth[AIR] = EARTH;
         
         int i = (this.depths[move] + 4) % 4;
-        while (i-- > 0 || this.elementsLeft[element] == 0) {
+        while (i-- > 0 || data.getElementsLeft(this.playerNumber, element) == 0) {
             element = onedepth[element];
         }
-        this.elementsLeft[element]--;
         
         return element;
     }
@@ -68,14 +61,11 @@ public class DepthEngine implements Player
     @Override
     public void onGameStart(Data gamedata, int yourPlayerNumber)
     {
-        Arrays.fill(this.elementsLeft, 2);
-        this.elementsLeft[DEFENSE] = 1;
     }
 
     @Override
     public void onMoveDone(int yourMove, int otherMove, int score)
     {
-        this.otherplayerelement = otherMove;
     }
 
     @Override
