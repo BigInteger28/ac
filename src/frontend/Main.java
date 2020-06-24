836b19f67fd672477ace662cb6917d9f6b2f5b8b
yugecin <yugecin@users.noreply.github.com>
2020-06-24 22:20:09 +0200

    Remove unneeded import

--- src/frontend/Main.java
+++ src/frontend/Main.java
@@ -23,7 +23,6 @@ import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
-import javax.swing.border.LineBorder;
 import javax.swing.border.TitledBorder;
 
 import java.awt.*;
